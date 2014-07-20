5f0c073c8796c34fd6008d427a4f9ae15e8eab8c
Matt Greenfield <gmattgreenfield@gmail.com>
2014-07-20 23:36:36 +0100

    moved sass to within jekyll files and renamed folders

--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,33 @@ var rename = require("gulp-rename");
 var jekyll = require('gulp-jekyll');
 
 
-// Jekyll Template
-gulp.task('jekyll', function () {
-    gulp.src(['./index.html', './_layouts/*.html', './_posts/*.{markdown,md}'])
-        .pipe(jekyll({
-            source: './templates/',
-            destination: './output/',
-            bundleExec: true
-        }))
-        .pipe(gulp.dest('./output/'));
-});
+// Jekyll Template - Not really working.....
+
+// gulp.task('jekyll', function () {
+//     gulp.src(['./index.html', './_layouts/*.html', './_posts/*.{markdown,md}'])
+//         .pipe(jekyll({
+//             source: 'jekyll-source/',
+//             destination: 'output/',
+//             bundleExec: true
+//         }))
+//         .pipe(gulp.dest('output/'));
+// });
 
 
 
-// Compile Our Sass
+// Compile Sass and minify it
 gulp.task('sass', function() {
-    return gulp.src('./styles/*.scss')
+    return gulp.src('jekyll-source/_sass/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./styles'));
-});
-
-// Minify CSS
-gulp.task('minify-css', function () {
-    gulp.src('styles/*.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('templates/assets/css/'));
+        .pipe(gulp.dest('jekyll-source/assets/css/'));
 });
 
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('styles/*.scss', ['sass', 'minify-css']);
+    gulp.watch('*.scss', ['sass']);
 });
 
 // Default Task
